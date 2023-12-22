9f112e57333dd5154dfc4dcd26862115f6f91643
KarolinaTad <k.t.1993@bk.ru>
2023-12-22 22:41:41 +0300

    Ex2 from Seminar 5 2rowArrays

---
+++ Ex2/Program.cs
@@ -0,0 +1,50 @@
+﻿//  Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] FillArray(int rows, int columns, int minValue, int MaxValue)
+{
+    int[,] array = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            array[i, l] = rand.Next(minValue, MaxValue + 1);
+
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(5, 5, 0, 10);
+PrintArray(array);
+
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(1); i++)
+{
+            int tmp = array[4, i];
+                array[4, i] = array[0, i];
+                array[0, i] = tmp;
+}
+ for(int i = 0; i < array.GetLength(0); i++)
+ {
+                for(int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
